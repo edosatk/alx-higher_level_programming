ea7011c0a715af9c9ca9d237c7a0640b5d8c425c
edosatk <gendabusen@gmail.com>
2022-08-31 21:47:28 +0000

    updated code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,5 @@
-nt(roman_string):
+#!/usr/bin/python3
+def roman_to_int(roman_string):
     """ converts a Roman numeral to an integer."""
     romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     if (roman_string is None) or (type(roman_string) is not str):
